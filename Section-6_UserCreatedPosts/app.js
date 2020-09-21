1a6d8efd4bcfd9a4b6cbfe7ec285cfa93a1037ea
SanketGawande27 <53644173+SanketGawande27@users.noreply.github.com>
2020-09-21 17:27:46 +0530

    Adding user post's into a DB

---
+++ Section-6_UserCreatedPosts/app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const session = require('express-session')
+const { response } = require('express')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('connect-flash')
+const app = express()
+
+let sessionOptions = session({
+    secret: "JavaScript is so cool",
+    store: new MongoStore({client: require('./db')}),
+    resave:false,
+    saveUninitialized: false,
+    cookie:{maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
+})
+
+app.use(sessionOptions)
+app.use(flash())
+
+app.use(function(req, res, next){
+    res.locals.user = req.session.user
+    next()
+})
+
+const router = require('./router')
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+
+app.use(express.static('public'))
+app.set('views','views')      //look views folder
+app.set('view engine', 'ejs')
+
+app.use('/', router)
+
+
+module.exports = app
