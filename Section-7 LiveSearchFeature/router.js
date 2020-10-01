9fad7472772cf5deb6f45bb97e2fab67ff2d5722
SanketGawande27 <53644173+SanketGawande27@users.noreply.github.com>
2020-10-01 19:41:13 +0530

    I have perform the following tasks 1. Responding to keypress event 2. Showing and hiding search overlay 3. Generating HTML for search result

---
+++ Section-7 LiveSearchFeature/router.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+const usreController = require('./controllers/userController')
+const postController = require('./controllers/postController')
+
+//user related routes
+router.get('/', usreController.home)
+router.post('/register', usreController.register)
+router.post('/login', usreController.login)
+router.post('/logout', usreController.logout)
+
+//profile related routes
+router.get('/profile/:username', usreController.ifUserExists, usreController.profilePostsScreen)
+
+//post related routes
+router.get('/create-post', usreController.mustBeLoggedIn, postController.viewCreateScreen)
+router.post('/create-post', usreController.mustBeLoggedIn, postController.create)
+router.get('/post/:id', postController.viewSingle)
+router.get('/post/:id/edit', usreController.mustBeLoggedIn, postController.viewEditScreen)
+router.post('/post/:id/edit', usreController.mustBeLoggedIn, postController.edit)
+router.post('/post/:id/delete', usreController.mustBeLoggedIn, postController.delete)
+router.post('/search', postController.search)
+
+module.exports =  router
