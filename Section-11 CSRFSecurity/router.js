d93e5e30a2709afc8e724b64be0afd8ae2c085c3
SanketGawande27 <53644173+SanketGawande27@users.noreply.github.com>
2020-10-06 12:12:24 +0530

    Added CSRF(Cross-Site Request Forgery)Security to our ComplaxApp.

---
+++ Section-11 CSRFSecurity/router.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+const usreController = require('./controllers/userController')
+const postController = require('./controllers/postController')
+const followController = require('./controllers/followController')
+
+//user related routes
+router.get('/', usreController.home)
+router.post('/register', usreController.register)
+router.post('/login', usreController.login)
+router.post('/logout', usreController.logout)
+router.post('/doesUsernameExist', usreController.doesUsernameExist)
+router.post('/doesEmailExist', usreController.doesEmailExist)
+
+//profile related routes
+router.get('/profile/:username', usreController.ifUserExists,usreController.sharedProfileData, usreController.profilePostsScreen)
+router.get('/profile/:username/followers', usreController.ifUserExists,usreController.sharedProfileData, usreController.profileFollowersScreen)
+router.get('/profile/:username/following', usreController.ifUserExists,usreController.sharedProfileData, usreController.profileFollowingScreen)
+
+//post related routes
+router.get('/create-post', usreController.mustBeLoggedIn, postController.viewCreateScreen)
+router.post('/create-post', usreController.mustBeLoggedIn, postController.create)
+router.get('/post/:id', postController.viewSingle)
+router.get('/post/:id/edit', usreController.mustBeLoggedIn, postController.viewEditScreen)
+router.post('/post/:id/edit', usreController.mustBeLoggedIn, postController.edit)
+router.post('/post/:id/delete', usreController.mustBeLoggedIn, postController.delete)
+router.post('/search', postController.search)
+
+//follow related routes
+router.post('/addFollow/:username', usreController.mustBeLoggedIn ,followController.addFollow)
+router.post('/removeFollow/:username', usreController.mustBeLoggedIn ,followController.removeFollow)
+
+module.exports =  router
